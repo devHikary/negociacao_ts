6691662a77927025d3214f83b9a5bc3112487220
devHikary <wrkkarina@gmail.com>
2021-09-17 07:08:21 -0300

    parâmetro opcional

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,14 +1,21 @@
 export abstract class View<T>{
   protected element: HTMLElement;
+  private escapar = false;
 
-  constructor(seletor: string) {
+  constructor(seletor: string, escapar?: boolean) {
     this.element = document.querySelector(seletor);
+    if(escapar){
+      this.escapar = escapar;
+    }
   }
 
   protected abstract template(model: T): string;
 
   update(model: T): void {
-    const template = this.template(model);
+    let template = this.template(model);
+    if(this.escapar){
+      template = template.replace(/<script>[\s\S]*?<\/script>/,'')
+    }
     this.element.innerHTML = template;
   }  
 }
