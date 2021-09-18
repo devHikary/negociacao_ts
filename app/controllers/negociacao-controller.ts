e3180dca745904223bde7fc0a58cf7472483577e
devHikary <wrkkarina@gmail.com>
2021-09-18 07:16:17 -0300

    decorator

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -3,6 +3,7 @@ import { NegociacoesView } from './../views/negociacoes-view.js';
 import { Negociacoes } from './../models/negociacoes.js';
 import { Negociacao } from '../models/negociacao.js';
 import { MensagemView } from '../views/mensagem-view.js';
+import { logarTempoDeExecucao } from '../decorators/logar-tempo-execucao.js';
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
@@ -19,6 +20,7 @@ export class NegociacaoController {
     this.negociacoesView.update(this.negocicoes);
   }
 
+  @logarTempoDeExecucao()
   public adiciona(): void {
     const negociacao = Negociacao.criaDe(
       this.inputData.value,
--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,8 +1,15 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 import { DiasDaSemana } from './../enums/dias-da-semana.js';
 import { NegociacoesView } from './../views/negociacoes-view.js';
 import { Negociacoes } from './../models/negociacoes.js';
 import { Negociacao } from '../models/negociacao.js';
 import { MensagemView } from '../views/mensagem-view.js';
+import { logarTempoDeExecucao } from '../decorators/logar-tempo-execucao.js';
 export class NegociacaoController {
     constructor() {
         this.negocicoes = new Negociacoes;
@@ -37,3 +44,6 @@ export class NegociacaoController {
         this.mensagemView.update('Negociação adicionada com sucesso');
     }
 }
+__decorate([
+    logarTempoDeExecucao()
+], NegociacaoController.prototype, "adiciona", null);
